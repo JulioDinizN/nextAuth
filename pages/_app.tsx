9d2475cacadf05a0c6990e2ca060e667dbfc3d48
JulioDinizN <juliocesardiniznogueira@gmail.com>
2021-12-07 21:56:25 -0300

    Project Done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthProvier } from '../contexts/AuthContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvier>
+      <Component {...pageProps} />
+    </AuthProvier>
+  )
 }
 
 export default MyApp
